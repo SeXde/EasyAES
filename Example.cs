cdc4c0a37b615d8bc8307fee61202cc018c36ae4
Álvaro Berdote <35387108+SeXde@users.noreply.github.com>
2022-04-04 19:02:45 +0200

    Add files via upload

---
+++ Example.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+// Example
+
+string message = "Don't trust mr five";
+string password = "Super secret password 100% real no fake";
+
+var tuple = EasyAES.Encrypt(message, password);
+string encrypted = tuple.Item1;
+byte[] iv = tuple.Item2;
+string decrypted = EasyAES.Decrypt(encrypted, password, iv);
+
+Console.WriteLine("The encrypted text is: {0}", encrypted);
+Console.WriteLine("The decrypted text is: {0}", decrypted);
+
